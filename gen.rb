d1c235a1b022f8a469308858867b14c0fc6c6bc8
alonzorozco <sfabian.orozco@gmail.com>
2021-05-19 22:16:23 -0400

    add: gen 2.0

--- gen.rb
+++ gen.rb
@@ -1,5 +1,5 @@
 
-
+# con arreglo
 #  numero = ARGV[0].to_i
 # def gen(n)  
 #     alfabeto = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
